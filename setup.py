3e42d90af486d383cc962b10b0d45098381c7eb2
TN3W <141021574+tn3w@users.noreply.github.com>
2023-08-17 09:31:56 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='flask_DDoSify',
-    version='0.9.4',
+    version='0.9.4.1',
     description='Protect against bots and DDoS attacks',
     long_description='A DDoS defense system for flask applications, first sends users to a captcha page without a javascript script and creates a confirmation cookie/url arg after the captcha.',
     author='TN3W',
