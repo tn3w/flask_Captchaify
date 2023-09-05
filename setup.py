58d08ccaa490819e265d25257291e52526e86d5f
TN3W <141021574+tn3w@users.noreply.github.com>
2023-09-05 14:05:26 +0000

    Updated classifiers

--- setup.py
+++ setup.py
@@ -14,12 +14,14 @@ setup(
         'flask_DDoSify': ['data/*', 'templates/*']
     },
     classifiers=[
+        'Environment :: Web Environment',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     keywords='flask, Python, Bot, Captcha, DDoS',
     install_requires=[
