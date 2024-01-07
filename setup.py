8b816bf5c4aa131446440adeba7508ee406cfba0
TN3W <tn3wA8xxfuVMs2@proton.me>
2024-01-07 12:08:50 +0100

    Fixed Error

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ try:
     from pip._internal.req import parse_requirements
 except ImportError:
     from pip.req import parse_requirements
-    
-requirements = [str(requirement.req) for requirement in parse_requirements("requirements.txt", session=False)]
+
+requirements = [str(requirement.requirement) for requirement in list(parse_requirements("requirements.txt", session=False))]
 
 setup(
     name='flask_Captchaify',
