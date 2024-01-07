d7f35a6193848c4b2da6ade52c0397ddd00c9119
TN3W <tn3wA8xxfuVMs2@proton.me>
2024-01-07 12:06:51 +0100

    Fixed Error

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 try:
-    from pip._internal.req import parse_requirements, download
+    from pip._internal.req import parse_requirements
 except ImportError:
     from pip.req import parse_requirements
-    from pip import download
-
-requirements = [str(requirement.req) for requirement in parse_requirements("requirements.txt", session=download.PipSession())]
+    
+requirements = [str(requirement.req) for requirement in parse_requirements("requirements.txt", session=False)]
 
 setup(
     name='flask_Captchaify',
