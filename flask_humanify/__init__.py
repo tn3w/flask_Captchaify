290404bb072460307ebdaefd7ad54684751052a7
tn3w <tn3w@protonmail.com>
2025-06-27 22:39:49 +0200

    Update version to 0.1.3 in __init__.py to reflect the latest changes.

--- flask_humanify/__init__.py
+++ flask_humanify/__init__.py
@@ -4,7 +4,7 @@ Flask-Humanify
 A Flask extension that protects against bots and DDoS attacks.
 """
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from . import utils
 from .humanify import Humanify
